cc6c965dd7df0c9a77f109201ac9f4fe79e0d3b7
SangHakLee <leesanghak7298@gmail.com>
2019-10-26 16:39:37 +0900

    모든 API에서 인증하도록 설정

---
+++ src/main/java/kr/codelabs/member/config/ResourceServerConfig.java
@@ -0,0 +1,17 @@
+package kr.codelabs.member.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+@EnableResourceServer // 인증 서버를 보통 Resource라 칭함
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated(); // 모든 요청이 인증이 필수, 예외 처리도 가능(찾아볼 것)
+    }
+}
